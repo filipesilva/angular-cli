f5019aa2cf97411765ee2d74d340b4f49d4e1a9e
Filipe Silva <filipematossilva@gmail.com>
2020-03-11 19:43:13 +0000

    build: build @angular-devkit/build-angular with Bazel

--- packages/ngtools/webpack/src/index.ts
+++ packages/ngtools/webpack/src/index.ts
@@ -11,3 +11,14 @@ export * from './interfaces';
 export { ngcLoader as default } from './loader';
 
 export const NgToolsLoader = __filename;
+
+// TODO: figure out way of not exporting this, and not getting bazel error on build_angular:
+// ERROR: /home/filipesilva/work/cli/packages/angular_devkit/build_angular/BUILD.bazel:55:1: Compiling TypeScri
+// pt (devmode) //packages/angular_devkit/build_angular:build_angular failed (Exit 1)
+// packages/angular_devkit/build_angular/src/angular-cli-files/plugins/webpack-rollup-loader.ts:17:44 - error T
+// S2307: Cannot find module '@ngtools/webpack/src/virtual_file_system_decorator'.
+
+// 17 import { VirtualFileSystemDecorator } from '@ngtools/webpack/src/virtual_file_system_decorator';
+//                                               ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+export { VirtualFileSystemDecorator } from './virtual_file_system_decorator';
